d7c6d81bb4b216a27cf8bcdaa8b402a2600c6d21
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-02-01 11:44:51 +0100

    Initial work

---
+++ default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    cmake
+    extra-cmake-modules
+    conan
+    clojure
+    go_1_10
+    maven
+    nodejs-10_x
+    openjdk
+    qt511.full
+    watchman
+    unzip
+    wget # 1.19.4
+    yarn
+  ];
+}
